db4b65e0ab3db18e9ef79a951b1ff8121aa9a418
SolijonovAsadbek <109437399+AsadbekSolijonov@users.noreply.github.com>
2025-05-29 19:44:37 +0500

    remove necessery modules

--- bot_registor/app.py
+++ bot_registor/app.py
@@ -3,11 +3,9 @@ import logging
 import sys
 from os import getenv
 
-from aiogram import Bot, Dispatcher, html, F
+from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
 from dotenv import load_dotenv
 
 # Bot token can be obtained via https://t.me/BotFather
